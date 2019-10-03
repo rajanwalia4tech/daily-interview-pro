5f1b275bfe93cab82a737d2ecd03531e69712e29
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-03 11:25:52 +0800

    Updated solution for 2019/10/03

--- 003_2019-10-03/solution.py
+++ 003_2019-10-03/solution.py
@@ -2,9 +2,9 @@
 class Solution: 
     def longestPalindrome(self, s):
         palindromes = []
-        for i, currentChar in enumerate(s):
-            for j in range(i+2, len(s)+1): # i + 2 due to Assumption 1
-                temp = s[i:j]
+        for i in range(len(s)):
+            for j in range(i+2, len(s)): # i + 2 due to Assumption 1
+                temp = s[i:j+1]
                 if temp == temp[::-1]:
                     palindromes.append(temp)
         result = max(palindromes, key=len) if palindromes else None
