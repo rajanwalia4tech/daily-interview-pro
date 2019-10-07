55e0e9a3040b865fbb758a3eba3b62577f89e055
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-07 17:52:55 +0800

    Added solution for 005

--- 005_First_and_Last_Indices_of_an_Element_in_a_Sorted_Array/solution.py
+++ 005_First_and_Last_Indices_of_an_Element_in_a_Sorted_Array/solution.py
@@ -1,7 +1,38 @@
 class Solution: 
   def getRange(self, arr, target):
-    # Fill this in.
-  
+    index = binary_search(arr, target)
+    if index == -1:
+      return [-1, -1]
+    
+    left = right = index
+
+    while left - 1 >= 0 and arr[left - 1] == target:
+      left = left - 1
+
+    while right + 1 < len(arr) and arr[right + 1] == target:
+      right = right + 1
+
+    return [left, right]
+
+def binary_search(arr, target):
+  low = 0
+  high = len(arr) - 1
+
+  while low <= high:
+    midPoint = (low + high) / 2
+    midPoint = int(midPoint)
+
+    current = arr[midPoint]
+    if current == target:
+      return midPoint
+    
+    if current < target:
+      low = midPoint + 1
+    else:
+      high = midPoint
+
+  return -1
+    
 # Test program 
 arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
 x = 2
