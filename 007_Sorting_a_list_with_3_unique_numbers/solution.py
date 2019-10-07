c0a9729c74a9f331deab721014302902a752fbb7
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-07 20:00:59 +0800

    Added solution for 007

--- 007_Sorting_a_list_with_3_unique_numbers/solution.py
+++ 007_Sorting_a_list_with_3_unique_numbers/solution.py
@@ -1,6 +1,13 @@
 def sortNums(nums):
-  # Fill this in.
-  pass
+  # merge sort/ quick sort etc best performance O (n log n)
+  # O(n)? counting sort/ radix sort
+  # list only will have (1,2,3), counting sort makes sense
+  countArr = [0 for _ in range(3)]
+  sortArr = []
+  for i in nums:  countArr[i-1] += 1
+  for i in range(3): sortArr.extend([i+1 for j in range(countArr[i])])
+  return sortArr
+  
 
 print (sortNums([3, 3, 2, 1, 3, 2, 1]))
 # [1, 1, 2, 2, 3, 3, 3]
