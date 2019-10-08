8391f37ddee794ed856b4fd0d00899aa4baa3ba2
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-08 23:19:39 +0800

    Added solution for 009

---
+++ 009_Find_the_Non-Duplicate_Number/solution.py
@@ -0,0 +1,17 @@
+def singleNumber(nums):
+    nums = sorted(nums)
+    previous = nums[0]
+    count = 1
+    for i in range(len(nums)-1):
+        if nums[i+1] == previous:
+            count += 1
+        elif nums[i+1] != previous:
+            if count != 2:
+                return previous
+            else:
+                count = 1
+                previous = nums[i+1]
+    return previous
+
+print (singleNumber([4, 3, 2, 4, 1, 3, 2]))
+# 1
