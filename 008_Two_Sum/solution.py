d5f99c866336b6279d4bd011cf04eec9ac30d31a
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-08 10:42:56 +0800

    Updated solution for 008

--- 008_Two_Sum/solution.py
+++ 008_Two_Sum/solution.py
@@ -1,10 +1,10 @@
 def two_sum(nums, k):
     seen = set()
     for num in nums:
-        seen.add(num)
         diff = k - num
         if diff in seen:
             return True
+        seen.add(num)
     return False
 
 print (two_sum([4,7,1,-3,2], 5))
