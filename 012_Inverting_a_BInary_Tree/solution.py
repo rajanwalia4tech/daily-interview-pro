495f455e9899b3dc56c00c424d6b233e4ef78e67
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-11 22:53:15 +0800

    Added 011 012 questions

---
+++ 012_Inverting_a_BInary_Tree/solution.py
@@ -0,0 +1,26 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print self.value,
+    if self.left: self.left.preorder()
+    if self.right: self.right.preorder()
+
+def invert(node):
+  # Fill this in.
+
+root = Node('a') 
+root.left = Node('b') 
+root.right = Node('c') 
+root.left.left = Node('d') 
+root.left.right = Node('e') 
+root.right.left = Node('f') 
+
+root.preorder()
+# a b d e c f 
+print "\n"
+invert(root)
+root.preorder()
+# a c f b e d
