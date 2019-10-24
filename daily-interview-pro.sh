dc19eb3601d84332f80eb3632f819737eb6efad2
Xin Zhe Khooi <khooi8913@gmail.com>
2019-10-24 17:24:50 +0800

    Added question adder script

---
+++ daily-interview-pro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function daily-interview-pro {
+	mkdir "$1";
+	cd "$1";
+	touch solution.py README.md;
+	echo "# $2" > README.md;
+}
+
+question=$2
+fileseq=$1
+filename=`echo $2 | sed 's/ /\_/g'`
+dirname=${fileseq}_${filename}
+echo "Created folder ${dirname}"
+daily-interview-pro $dirname $question
+
+
